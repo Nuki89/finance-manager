28d94e126b90fc8612975caa767e55f891fa49f7
rud0lph89 <z.spiler46@gmail.com>
2025-01-05 22:32:25 +0100

    feat: added finance_api app/updated settings/urls

---
+++ backend/finance_api/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework.permissions import IsAuthenticated
+from .views import *
+from . import views
+
+class BaseRouterRootView(routers.APIRootView):
+    # permission_classes = [IsAuthenticated]
+    def get_view_name(self) -> str:
+        return "Finance API"
+
+class BaseRouter(routers.DefaultRouter):
+    APIRootView = BaseRouterRootView
+
+router = BaseRouter()
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
