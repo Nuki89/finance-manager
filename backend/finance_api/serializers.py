62b1562ddf9c448b220ad6bc1a44966d7a92121f
rud0lph89 <z.spiler46@gmail.com>
2025-01-06 22:55:09 +0100

    feat: added income serilizer/ updated to hyperlink

--- backend/finance_api/serializers.py
+++ backend/finance_api/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 from .models import *
 
-class IncomeSourceSerializer(serializers.ModelSerializer):
+class IncomeSourceSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = IncomeSource
+        fields = '__all__'
+
+
+class IncomeSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+
+    source = serializers.PrimaryKeyRelatedField(queryset=IncomeSource.objects.all())
+    source_data = IncomeSourceSerializer(source='source', read_only=True)
+
+    class Meta:
+        model = Income
         fields = '__all__'
