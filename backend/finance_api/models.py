2f976538ceb65bf76e6f25ba1745930c81ad57db
rud0lph89 <z.spiler46@gmail.com>
2025-01-07 22:30:06 +0100

    fix: updated description null

--- backend/finance_api/models.py
+++ backend/finance_api/models.py
@@ -12,7 +12,7 @@ class Income(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     source = models.ForeignKey(IncomeSource, on_delete=models.DO_NOTHING)
     date = models.DateField(default=get_current_date)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return f'{self.amount} from {self.source.name} on {self.date}'
