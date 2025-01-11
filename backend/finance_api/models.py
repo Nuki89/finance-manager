0338d44ea3e72ffc06bc9867dd0058b3210fe329
rud0lph89 <z.spiler46@gmail.com>
2025-01-11 23:59:54 +0100

    feat: updated tables/added expense categories

--- backend/finance_api/models.py
+++ backend/finance_api/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from .utils import *
 
+
 class IncomeSource(models.Model):
     name = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'income_sources'
 
     def __str__(self):
         return self.name
@@ -13,6 +17,19 @@ class Income(models.Model):
     source = models.ForeignKey(IncomeSource, on_delete=models.DO_NOTHING)
     date = models.DateField(default=get_current_date)
     description = models.TextField(null=True, blank=True)
+    
+    class Meta:
+        db_table = 'incomes'
 
     def __str__(self):
         return f'{self.amount} from {self.source.name} on {self.date}'
+
+
+class ExpenseCategory(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'expense_categories'
+
+    def __str__(self):
+        return self.name
