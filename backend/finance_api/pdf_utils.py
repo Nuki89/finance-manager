223ae5261ffd1d47062cead1129306b767925450
rud0lph89 <z.spiler46@gmail.com>
2025-01-12 23:48:40 +0100

    feat: updated income pdf

--- backend/finance_api/pdf_utils.py
+++ backend/finance_api/pdf_utils.py
@@ -1,103 +1,100 @@
 from reportlab.lib.pagesizes import letter
 from reportlab.lib import colors
 from reportlab.pdfgen import canvas
+from reportlab.graphics.shapes import Drawing
+from reportlab.graphics.charts.piecharts import Pie
 from django.http import HttpResponse
+from datetime import datetime
+import random
+import string
+
+
+def random_filename(suffix=".pdf", length=20):
+    characters = string.ascii_letters + string.digits  
+    random_string = ''.join(random.choice(characters) for i in range(length))
+    return f"{random_string}{suffix}"
+
+
+def draw_income_headers(p, y, width):
+    p.setFont('Helvetica-Bold', 10)
+    p.drawString(50, y, 'Date')
+    p.drawString(170, y, 'Source')
+    p.drawString(280, y, 'Amount')
+    p.drawString(390, y, 'Description')
+    p.drawString(500, y, 'Comment')
+
+    p.line(50, y - 4, width - 50, y - 4)
+    y -= 22
+    return y
+
 
 def generate_incomes_pdf(incomes):
+    filename = random_filename()
     response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="incomes-report.pdf"'
+    response['Content-Disposition'] = f'attachment; filename="{filename}"'
     p = canvas.Canvas(response, pagesize=letter)
-    p.setTitle("Income Report")
+    p.setTitle("Income Summary")
     
     width, height = letter
     p.setFont('Helvetica-Bold', 14)
     p.setFillColor(colors.darkblue)
-    p.drawCentredString(width / 2.0, height - 50, "Income Report")
+    p.drawCentredString(width / 2.0, height - 50, "Income Summary")
 
-    y = height - 80
-    p.setFont('Helvetica-Bold', 12)
-    p.drawString(100, y, 'Date')
-    p.drawString(200, y, 'Source')
-    p.drawString(350, y, 'Amount')
-    p.drawString(450, y, 'Description')
-    y -= 20
+    current_date = datetime.now().strftime('%d.%m.%Y')
     p.setFont('Helvetica', 10)
+    p.setFillColor(colors.grey)
+    p.drawString(440, height - 80, f"Date of extract: {current_date}")
+
     p.setFillColor(colors.black)
+    y = height - 80
+    
+    p.setFont('Helvetica-Bold', 10)
+    p.drawString(50, y, 'CURRENCY: EUR')
+    p.line(50, y - 4, width - 50, y - 4)
+
+    y = height - 100
+    y = draw_income_headers(p, y, width)
+
+    p.setFont('Helvetica', 8)
 
     for income in incomes:
         if y < 100:
             p.showPage()
             y = height - 80
-            p.setFont('Helvetica-Bold', 12)
-            p.drawString(100, y, 'Date')
-            p.drawString(200, y, 'Source')
-            p.drawString(350, y, 'Amount')
-            p.drawString(450, y, 'Description')
-            y -= 20
-            p.setFont('Helvetica', 10)
+            y = draw_income_headers(p, y, width)
 
         description = income.description if income.description.strip() else "No description"
         formatted_date = income.date.strftime('%d.%m.%Y')  
-        p.drawString(100, y, formatted_date)
-        p.drawString(200, y, str(income.source.name))
-        p.drawString(350, y, "€{:,.2f}".format(income.amount))
-        p.drawString(450, y, description)
-        y -= 20
+        p.drawString(50, y, formatted_date)
+        p.drawString(170, y, str(income.source.name))
+        p.drawString(280, y, "{:,.2f} €".format(income.amount))
+        p.drawString(390, y, description)
+
+        p.line(50, y - 4, width - 50, y - 4)
+
+        y -= 22
 
     p.showPage()
     p.save()
     return response
 
-    # y = height - 80
-    # p.setFont('Helvetica-Bold', 12)
-    # p.drawString(100, y, 'VALUTA: EUR')
 
-# CHANGED ORDER OF COLUMNS IN PDF
-# from reportlab.lib.pagesizes import letter
-# from reportlab.lib import colors
-# from reportlab.pdfgen import canvas
-# from django.http import HttpResponse
+# PIE
+    # # Prepare data for the pie chart
+    # data = [float(holding.amount) for holding in incomes]
+    # labels = [holding.source.name for holding in incomes]
 
-# def generate_incomes_pdf(incomes):
-#     response = HttpResponse(content_type='application/pdf')
-#     response['Content-Disposition'] = 'attachment; filename="incomes-report.pdf"'
-#     p = canvas.Canvas(response, pagesize=letter)
-#     p.setTitle("Income Report")
-    
-#     width, height = letter
-#     p.setFont('Helvetica-Bold', 14)
-#     p.setFillColor(colors.darkblue)
-#     p.drawCentredString(width / 2.0, height - 50, "Income Report")
+    # # Create a drawing object
+    # d = Drawing(400, 200)
+    # pie = Pie()
+    # pie.x = 150
+    # pie.y = 50
+    # pie.data = data
+    # pie.labels = labels
+    # pie.slices.strokeWidth=0.5
+    # pie.slices.strokeColor=colors.black
 
-#     y = height - 80
-#     p.setFont('Helvetica-Bold', 12)
-#     p.drawString(100, y, 'Source')
-#     p.drawString(250, y, 'Amount')
-#     p.drawString(350, y, 'Description')
-#     p.drawString(500, y, 'Date')
-#     y -= 20
-#     p.setFont('Helvetica', 10)
-#     p.setFillColor(colors.black)
+    # d.add(pie)
 
-#     for income in incomes:
-#         if y < 100:
-#             p.showPage()
-#             y = height - 80
-#             p.setFont('Helvetica-Bold', 12)
-#             p.drawString(100, y, 'Source')
-#             p.drawString(250, y, 'Amount')
-#             p.drawString(350, y, 'Description')
-#             p.drawString(500, y, 'Date')
-#             y -= 20
-#             p.setFont('Helvetica', 10)
-
-#         description = income.description if income.description.strip() else "No description"
-#         p.drawString(100, y, str(income.source.name))
-#         p.drawString(250, y, "${:,.2f}".format(income.amount))
-#         p.drawString(350, y, description)
-#         p.drawString(500, y, str(income.date))
-#         y -= 20
-
-#     p.showPage()
-#     p.save()
-#     return response
+    # # Draw the pie chart at specified location
+    # d.drawOn(p, 50, height - 300)
