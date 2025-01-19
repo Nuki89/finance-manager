da6b74c3c0e2bdbb5526a23d52d6fb8c4bdea100
rud0lph89 <z.spiler46@gmail.com>
2025-01-19 23:03:03 +0100

    feat: added incomes to home route

--- frontend/src/app/routes/home/home.component.ts
+++ frontend/src/app/routes/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { IncomesComponent } from "../transactions/incomes/incomes.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [IncomesComponent, IncomesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
