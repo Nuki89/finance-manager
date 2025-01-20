ea49e4101121eff44da22999f8188626bb000350
rud0lph89 <z.spiler46@gmail.com>
2025-01-20 12:25:40 +0100

    feat: income component connected to backend

--- frontend/src/app/routes/transactions/incomes/incomes.component.ts
+++ frontend/src/app/routes/transactions/incomes/incomes.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { IncomeService } from '../../../shared/services/api/income.service';
 
 @Component({
   selector: 'app-incomes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './incomes.component.html',
-  styleUrl: './incomes.component.css'
+  styleUrls: ['./incomes.component.css']
 })
 export class IncomesComponent {
+  incomes: any = []
 
+  constructor(private incomeService: IncomeService) { }
+
+  ngOnInit() {
+    this.incomeService.getIncome().subscribe((data: any) => {
+      this.incomes = data
+      console.log(this.incomes)
+    })
+  }
 }
