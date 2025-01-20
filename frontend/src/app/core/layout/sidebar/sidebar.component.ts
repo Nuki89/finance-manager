c2749ff0ca91650a00b1b70767cb33ca9b5db533
rud0lph89 <z.spiler46@gmail.com>
2025-01-20 19:34:01 +0100

    feat: added new icons/updated sidebar

--- frontend/src/app/core/layout/sidebar/sidebar.component.ts
+++ frontend/src/app/core/layout/sidebar/sidebar.component.ts
@@ -1,12 +1,15 @@
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { bootstrapLayoutSidebar, bootstrapXLg } from '@ng-icons/bootstrap-icons';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
+  viewProviders : [provideIcons({ bootstrapLayoutSidebar, bootstrapXLg})]
 })
 export class SidebarComponent {
 
-}
+} 
