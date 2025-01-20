81efaddb8ac17a97e504f6d1d060081dabfb64d6
rud0lph89 <z.spiler46@gmail.com>
2025-01-20 17:02:18 +0100

    update: login form (basic)

--- frontend/src/app/routes/auth/login/login.component.ts
+++ frontend/src/app/routes/auth/login/login.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../shared/services/auth/auth.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  imports: [CommonModule, FormsModule],
+  template: `
+    <form (ngSubmit)="onSubmit()">
+      <label for="username">Username:</label>
+      <input type="text" [(ngModel)]="username" name="username" required />
+
+      <label for="password">Password:</label>
+      <input type="password" [(ngModel)]="password" name="password" required />
+
+      <button type="submit">Login</button>
+    </form>
+  `
 })
 export class LoginComponent {
+  username = '';
+  password = '';
 
+  constructor(
+    private authService: AuthService,
+  ) {}
+
+  onSubmit(): void {
+    this.authService.login(this.username, this.password).subscribe(
+      () => {
+        console.log('Login successful');
+      },
+      error => {
+        console.error('Login failed:', error);
+      }
+    );
+  }
 }
