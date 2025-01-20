9e4145e5afde41dd092cb530f3d4e0a80580f13f
rud0lph89 <z.spiler46@gmail.com>
2025-01-20 17:07:57 +0100

    update: routes added authGuard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,28 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './routes/home/home.component';
+import { LoginComponent } from './routes/auth/login/login.component';
+import { authGuard } from './shared/services/auth/auth.guard';
+import { IncomesComponent } from './routes/transactions/incomes/incomes.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-];
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    {
+      path: 'home',
+      component: HomeComponent,
+      canActivate: [authGuard],
+      children: [
+        { path: 'incomes', component: IncomesComponent }
+      ]
+    },
+
+    { path: '**', redirectTo: '/login' },
+  ];
+  
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule {}
+
