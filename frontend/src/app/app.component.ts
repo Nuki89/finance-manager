79454617eae57f02791af8e0a0f9d1172d9cdb77
rud0lph89 <z.spiler46@gmail.com>
2025-01-20 17:08:18 +0100

    update: app, added loading

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { AuthService } from './shared/services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'finance-manager';
+  loading = false;
+
+  constructor(private router: Router, private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.loading = true;
+    if (this.authService.isLoggedIn()) {
+      this.router.navigate(['home']);
+    } else {
+      this.router.navigate(['/login']);
+    }
+    this.loading = false;
+  }
+  
 }
