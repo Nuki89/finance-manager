b9c7edfca85ee93c65e056a11fad37afbc5b179b
rud0lph89 <z.spiler46@gmail.com>
2025-01-22 00:11:27 +0100

    feat: added logut button connection to header

--- frontend/src/app/core/layout/header/header.component.ts
+++ frontend/src/app/core/layout/header/header.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../../shared/services/auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -15,4 +16,14 @@ export class HeaderComponent {
   firstName: string = 'Nuki';
   lastName: string = 'Pipika';
   email: string = 'nuki@gmail.com';
+
+  constructor(
+    private authService: AuthService,
+  ) {}
+
+  onLogout(): void {
+    this.authService.logout()
+    console.log('Logout successful');
+  }
+
 }
