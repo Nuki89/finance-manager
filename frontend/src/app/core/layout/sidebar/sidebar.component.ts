60adf528ebbaa04e77b2c2b25c2e690f1e3c5365
rud0lph89 <z.spiler46@gmail.com>
2025-01-21 20:08:45 +0100

    feat: added hugeicons/implemented into sidebar

--- frontend/src/app/core/layout/sidebar/sidebar.component.ts
+++ frontend/src/app/core/layout/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { bootstrapLayoutSidebar, bootstrapXLg } from '@ng-icons/bootstrap-icons';
+import { hugeMoneyAdd02, hugeMoneyRemove02 } from '@ng-icons/huge-icons';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,7 +9,7 @@ import { Component } from '@angular/core';
   imports: [NgIcon],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
-  viewProviders : [provideIcons({ bootstrapLayoutSidebar, bootstrapXLg})]
+  viewProviders : [provideIcons({ bootstrapLayoutSidebar, bootstrapXLg, hugeMoneyAdd02, hugeMoneyRemove02})]
 })
 export class SidebarComponent {
 
