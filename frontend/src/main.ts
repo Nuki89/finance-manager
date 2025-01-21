956ac2cf6454b43313d80f8ea9b08ce0d7860374
rud0lph89 <z.spiler46@gmail.com>
2025-01-21 19:32:56 +0100

    feat-test: added prod mode/environment.prod.ts+main.ts updated

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -11,6 +11,14 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { authInterceptor } from './app/shared/services/auth/auth.interceptor';
+// import { enableProdMode } from '@angular/core';
+// import { environment } from './environments/environment.prod';
+
+// if (environment.production) {
+//   enableProdMode();
+// }
+
+// ENABLE PROD MODE !! we I need to test this soulution !!
 
 bootstrapApplication(AppComponent, {
   providers: [
