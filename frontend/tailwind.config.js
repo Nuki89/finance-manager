55ee668bb0298419c97bc5d01ad93671837f6ff3
rud0lph89 <z.spiler46@gmail.com>
2025-01-22 13:41:56 +0100

    update: tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
 		"./node_modules/flowbite/**/*.js"
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					600: '#2563eb',
+					700: '#1d4ed8',
+					300: '#93c5fd',
+					800: '#1e3a8a',
+				},
+			},
+		},
 	},
 	prefix: '',
 	// prefix: 'tw-',
