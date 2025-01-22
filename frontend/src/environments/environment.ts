436efa98289708322b011a1316527eec62a65946
rud0lph89 <z.spiler46@gmail.com>
2025-01-22 22:31:51 +0100

    update: added few more endpoints to environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,4 +4,10 @@ export const apiEndpoints = {
     production: false,
     apiUrlListIncomes: `${BASE_URL}/incomes/`,
     apiUrlListIncomeSources: `${BASE_URL}/income-sources/`,
+    apiUrlListExpenses: `${BASE_URL}/expenses/`,
+    apiUrlListExpenseCategories: `${BASE_URL}/expense-categories/`,
+    apiUrlListSavings: `${BASE_URL}/savings/`,
+    apiUrlListSavingCategories: `${BASE_URL}/saving-categories/`,
+    apiUrlExportingPdf: `${BASE_URL}/exporting-pdf/`,
+    apiUrlBalance: `${BASE_URL}/balance/`,
 };
