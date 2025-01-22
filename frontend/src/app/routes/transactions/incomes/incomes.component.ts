1b6aedbe7f6a3c001f31293710e82b024264cec7
rud0lph89 <z.spiler46@gmail.com>
2025-01-22 21:07:17 +0100

    feat: added income form/connected to incomes view

--- frontend/src/app/routes/transactions/incomes/incomes.component.ts
+++ frontend/src/app/routes/transactions/incomes/incomes.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { IncomeService } from '../../../shared/services/api/income.service';
 import { IncomeTableComponent } from "./income-table/income-table.component";
+import { IncomeFormComponent } from './income-form/income-form.component';
 
 @Component({
   selector: 'app-incomes',
   standalone: true,
-  imports: [CommonModule, RouterModule, IncomeTableComponent],
+  imports: [CommonModule, RouterModule, IncomeTableComponent, IncomeFormComponent],
   templateUrl: './incomes.component.html',
   styleUrls: ['./incomes.component.css']
 })
