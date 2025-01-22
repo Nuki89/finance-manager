0e1d0206c147825c1848cb2b5d9a13883af4fb2d
rud0lph89 <z.spiler46@gmail.com>
2025-01-22 13:42:07 +0100

    update: login style

--- frontend/src/app/routes/auth/login/login.component.ts
+++ frontend/src/app/routes/auth/login/login.component.ts
@@ -1,13 +1,16 @@
+import { NgIcon, provideIcons } from '@ng-icons/core';
 import { Component } from '@angular/core';
 import { AuthService } from '../../../shared/services/auth/auth.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { cryptoBtc } from '@ng-icons/cryptocurrency-icons';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, NgIcon],
   templateUrl: './login.component.html',
+  viewProviders : [provideIcons({ cryptoBtc })]
 })
 export class LoginComponent {
   username = '';
