10481694afb3a48beb99a4bd69d62b053d87abb1
rud0lph89 <z.spiler46@gmail.com>
2025-01-25 23:16:59 +0100

    feat: updated barchart component

--- frontend/src/app/shared/ui/charts/bar-chart.component.ts
+++ frontend/src/app/shared/ui/charts/bar-chart.component.ts
@@ -1,16 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Chart, ChartConfiguration, registerables } from 'chart.js';
 
 @Component({
   selector: 'app-bar-chart',
   standalone: true,
-  imports: [],
-  template: `
-    <p>
-      bar-chart works!
-    </p>
-  `,
-  styles: ``
+  template: `<canvas #chartCanvas></canvas>`,
+  styles: [`
+    canvas {
+      max-width: 100%;
+      margin: auto;
+    }
+  `]
 })
-export class BarChartComponent {
+export class BarChartComponent implements OnInit {
+  @Input() data: { labels: string[]; datasets: { label: string; data: number[]; backgroundColor: string }[] } = {
+    labels: [],
+    datasets: [],
+  };
+  @ViewChild('chartCanvas', { static: true }) chartCanvas!: ElementRef<HTMLCanvasElement>;
 
+  constructor() {
+    Chart.register(...registerables);
+  }
+
+  ngOnInit() {
+    this.renderChart();
+  }
+
+  renderChart() {
+    const config: ChartConfiguration = {
+      type: 'bar',
+      data: this.data,
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+        },
+        scales: {
+          x: {
+            beginAtZero: true,
+          },
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    };
+
+    new Chart(this.chartCanvas.nativeElement, config);
+  }
 }
