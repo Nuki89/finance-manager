92eb806a902ef395739a23ad57b18270efba5ba1
rud0lph89 <z.spiler46@gmail.com>
2025-01-29 22:26:39 +0100

    feat: added datepicker

---
+++ frontend/src/app/shared/ui/components/datepicker/datepicker.component.ts
@@ -0,0 +1,40 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Output, ViewEncapsulation } from '@angular/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule, DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
+
+export const MY_DATE_FORMAT = {
+  parse: {
+    dateInput: 'DD/MM/YYYY', 
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY', 
+    monthYearLabel: 'MMMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY'
+  }
+};
+
+@Component({
+  selector: 'app-datepicker',
+  standalone: true,
+  providers: [
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    { provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMAT }
+  ],
+  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './datepicker.component.html',
+  styleUrls: ['./datepicker.component.css'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class DatepickerComponent {
+  @Output() dateChange = new EventEmitter<Date>();
+
+  onDateSelected(event: any) {
+    const selectedDate = event.value;
+    this.dateChange.emit(selectedDate);
+  }
+}
