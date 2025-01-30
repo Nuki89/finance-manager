f231b9dbdda1f29d37390ebf3925fb9853a3f456
rud0lph89 <z.spiler46@gmail.com>
2025-01-31 00:20:25 +0100

    feat: added particles update background

--- frontend/src/app/core/layout/main-layout/main-layout.component.ts
+++ frontend/src/app/core/layout/main-layout/main-layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
+import { ParticlesBackgroundComponent } from '../../../shared/ui/components/particles-background/particles-background.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterModule, HeaderComponent],
+  imports: [RouterModule, HeaderComponent, ParticlesBackgroundComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })
