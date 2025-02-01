bb33829675fdeac6f9dba98fe00c6d91de5f693b
rud0lph89 <z.spiler46@gmail.com>
2025-02-01 19:22:23 +0100

    feat: added register form/fewtest in login//not working errorsgit add .

--- frontend/src/app/routes/auth/login/login.component.ts
+++ frontend/src/app/routes/auth/login/login.component.ts
@@ -5,17 +5,20 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { cryptoBtc } from '@ng-icons/cryptocurrency-icons';
 import { ToastrService } from 'ngx-toastr';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, NgIcon],
+  imports: [CommonModule, FormsModule, NgIcon, RouterModule],
   templateUrl: './login.component.html',
   viewProviders : [provideIcons({ cryptoBtc })]
 })
 export class LoginComponent {
+  title = "Coiny"
   username = '';
   password = '';
+  isInvalid: boolean = false;
 
   constructor(
     private authService: AuthService,
@@ -32,9 +35,13 @@ export class LoginComponent {
   onSubmit(): void {
     this.authService.login(this.username, this.password).subscribe(
       () => {
-        this.toastr.success('You have successfully logged in.','Login successful');
+        this.isInvalid = true;
+        console.log(this.isInvalid);
+        // this.toastr.success('You have successfully logged in.','Login successful');
       },
       error => {
+        this.isInvalid = false;
+        console.log(this.isInvalid);
         this.toastr.error('Please check your username or password.', 'Login Failed');
       }
     );
--- frontend/src/app/routes/auth/register/register.component.ts
+++ frontend/src/app/routes/auth/register/register.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { cryptoBtc } from '@ng-icons/cryptocurrency-icons';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterModule, NgIcon],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css'],
+  viewProviders : [provideIcons({ cryptoBtc })]
 })
 export class RegisterComponent {
-
+  title = "Coiny"
 }
