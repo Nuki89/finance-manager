4f3ec45c69bc1f097ca7e8d524ccee94a12235cc
rud0lph89 <z.spiler46@gmail.com>
2025-02-01 22:54:25 +0100

    feat: added terms component

---
+++ frontend/src/app/routes/auth/terms-policy/terms-policy.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-terms-policy',
+  standalone: true,
+  imports: [CommonModule, RouterModule, FormsModule],
+  templateUrl: './terms-policy.component.html',
+  styleUrl: './terms-policy.component.css'
+})
+export class TermsPolicyComponent {
+
+}
