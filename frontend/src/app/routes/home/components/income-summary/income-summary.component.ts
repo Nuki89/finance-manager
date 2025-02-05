ce2027a6cc2f8fff9755ec0c83d7ead52cbde521
rud0lph89 <z.spiler46@gmail.com>
2025-02-05 21:30:57 +0100

    feat: home divided to income summary component

---
+++ frontend/src/app/routes/home/components/income-summary/income-summary.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import { IncomeService } from '../../../../shared/services/api/income.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ToggleViewService } from '../../../../shared/services/shared/toggle-view.service';
+
+@Component({
+  selector: 'app-income-summary',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './income-summary.component.html',
+  styleUrl: './income-summary.component.css'
+})
+export class IncomeSummaryComponent {
+  loading: boolean = true;
+  totalIncome: number = 0;
+  incomeSources: { name: string; value: number }[] = [];
+  selectedView: string = 'Month';
+  viewSubscription: any;
+  
+
+  constructor(
+    private incomeService: IncomeService, 
+    private toggleViewService: ToggleViewService
+  ) {}
+
+  ngOnInit() {
+    this.viewSubscription = this.toggleViewService.viewMode$.subscribe(view => {
+      this.selectedView = view;
+      this.fetchIncomeData();
+    });
+
+  }
+
+  fetchIncomeData(): void {
+    this.loading = true; 
+
+    const totalIncomeFetcher =
+      this.selectedView === 'Yearly'
+        ? this.incomeService.getLastYearIncome()
+        : this.incomeService.getLastMonthIncome();
+
+    totalIncomeFetcher.subscribe(
+      (data: any) => {
+        this.totalIncome = data.reduce((sum: any, item: { total_amount: any }) => sum + item.total_amount, 0);
+        this.checkLoadingComplete();
+      },
+      (error) => {
+        console.error(`Error fetching ${this.selectedView} total income:`, error);
+        this.totalIncome = 0;
+        this.checkLoadingComplete();
+      }
+    );
+  }
+
+  checkLoadingComplete(): void {
+    if (this.incomeSources.length > 0 || this.totalIncome !== 0) {
+      this.loading = false;
+    }
+  }
+
+}
