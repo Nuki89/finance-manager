11467c3db9cee0bc7334249de645f958ab486b37
rud0lph89 <z.spiler46@gmail.com>
2025-02-06 22:33:39 +0100

    feat: updated auth guard/no more session error

--- frontend/src/app/shared/services/auth/auth.guard.ts
+++ frontend/src/app/shared/services/auth/auth.guard.ts
@@ -1,20 +1,38 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService } from './auth.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class authGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService, 
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: any
+  ) {}
 
   canActivate(): boolean {
-    const isLoggedIn = this.authService.isLoggedIn();
-    if (isLoggedIn) {
-      return true;
-    } else {
+    if (!isPlatformBrowser(this.platformId)) {
+      return false;
+    }
+    if (!this.authService.isLoggedIn()) {
       this.router.navigate(['/login']);
       return false;
     }
+    return true;
   }
+
+  // canActivate(): boolean {
+  //   const isLoggedIn = this.authService.isLoggedIn();
+  //   if (isLoggedIn) {
+  //     return true;
+  //   } else {
+  //     this.router.navigate(['/login']);
+  //     return false;
+  //   }
+  // }
+
+  
 }
