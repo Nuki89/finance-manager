c0b7e1cd50fe519447d6ad60d310e20cca03ac91
rud0lph89 <z.spiler46@gmail.com>
2025-02-26 23:28:56 +0100

    fix: added isPlatformBrowser to darkmode

--- frontend/src/app/shared/services/shared/dark-mode.service.ts
+++ frontend/src/app/shared/services/shared/dark-mode.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,14 @@ export class DarkModeService {
   
   darkMode$ = this.darkModeSubject.asObservable();
 
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {}
+
   toggleDarkMode() {
     const currentMode = this.darkModeSubject.value;
     this.darkModeSubject.next(!currentMode);
-    localStorage.setItem('darkMode', JSON.stringify(!currentMode));
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('darkMode', JSON.stringify(!currentMode));
+    }
   }
 
   getCurrentState(): boolean {
@@ -20,8 +25,39 @@ export class DarkModeService {
   }
 
   private getSavedState(): boolean {
-    const storedState = localStorage.getItem('darkMode');
-    return storedState !== null ? JSON.parse(storedState) : false;
+    if (isPlatformBrowser(this.platformId)) {
+      const storedState = localStorage.getItem('darkMode');
+      return storedState !== null ? JSON.parse(storedState) : false;
+    }
+    return false;  
   }
-  
 }
+
+
+// import { Injectable } from '@angular/core';
+// import { BehaviorSubject } from 'rxjs';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class DarkModeService {
+//   private darkModeSubject = new BehaviorSubject<boolean>(this.getSavedState());
+  
+//   darkMode$ = this.darkModeSubject.asObservable();
+
+//   toggleDarkMode() {
+//     const currentMode = this.darkModeSubject.value;
+//     this.darkModeSubject.next(!currentMode);
+//     localStorage.setItem('darkMode', JSON.stringify(!currentMode));
+//   }
+
+//   getCurrentState(): boolean {
+//     return this.darkModeSubject.value;
+//   }
+
+//   private getSavedState(): boolean {
+//     const storedState = localStorage.getItem('darkMode');
+//     return storedState !== null ? JSON.parse(storedState) : false;
+//   }
+  
+// }
