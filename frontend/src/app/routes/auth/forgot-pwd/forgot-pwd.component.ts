01b617a89dc9ef33e2613e620c51834552d31df4
rud0lph89 <z.spiler46@gmail.com>
2025-02-28 16:58:49 +0100

    update auth forgot pwd html

--- frontend/src/app/routes/auth/forgot-pwd/forgot-pwd.component.ts
+++ frontend/src/app/routes/auth/forgot-pwd/forgot-pwd.component.ts
@@ -2,13 +2,16 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { cryptoBtc } from '@ng-icons/cryptocurrency-icons';
 
 @Component({
   selector: 'app-forgot-pwd',
   standalone: true,
-  imports: [CommonModule, RouterModule, FormsModule],
+  imports: [CommonModule, RouterModule, FormsModule, NgIcon],
   templateUrl: './forgot-pwd.component.html',
-  styleUrl: './forgot-pwd.component.css'
+  styleUrls: ['./forgot-pwd.component.css'],
+  providers: [provideIcons({ cryptoBtc })]
 })
 export class ForgotPwdComponent {
   title = "Coiny"
