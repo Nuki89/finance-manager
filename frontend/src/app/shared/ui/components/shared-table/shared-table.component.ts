bbb5a6ab3eb5cb66a1b159e5f8088af05db35d61
rud0lph89 <z.spiler46@gmail.com>
2025-02-28 17:13:09 +0100

    feat: created shared table component

---
+++ frontend/src/app/shared/ui/components/shared-table/shared-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shared-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './shared-table.component.html',
+  styleUrls: ['./shared-table.component.css']
+})
+export class SharedTableComponent {
+
+}
