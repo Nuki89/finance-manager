b5fa4b7fc83ad2978bf0f059d557337ced20c4fe
rud0lph89 <z.spiler46@gmail.com>
2025-03-01 19:01:47 +0100

    feat: update barchart to handle data

--- frontend/src/app/shared/ui/charts/bar-chart.component.ts
+++ frontend/src/app/shared/ui/charts/bar-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { Chart, ChartConfiguration, registerables } from 'chart.js';
 
 @Component({
@@ -13,12 +13,12 @@ import { Chart, ChartConfiguration, registerables } from 'chart.js';
     }
   `]
 })
-export class BarChartComponent implements OnInit {
-  @Input() data: { labels: string[]; datasets: { label: string; data: number[]; backgroundColor: string }[] } = {
-    labels: [],
-    datasets: [],
-  };
+export class BarChartComponent implements OnInit, OnChanges {
+  @Input() data!: { labels: string[]; datasets: { label: string; data: number[]; backgroundColor: string }[] };
+
   @ViewChild('chartCanvas', { static: true }) chartCanvas!: ElementRef<HTMLCanvasElement>;
+
+  private chart!: Chart;
 
   constructor() {
     Chart.register(...registerables);
@@ -26,6 +26,12 @@ export class BarChartComponent implements OnInit {
 
   ngOnInit() {
     this.renderChart();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['data'] && this.chart) {
+      this.updateChart(); 
+    }
   }
 
   renderChart() {
@@ -41,16 +47,77 @@ export class BarChartComponent implements OnInit {
           },
         },
         scales: {
-          x: {
-            beginAtZero: true,
-          },
-          y: {
-            beginAtZero: true,
-          },
+          x: { beginAtZero: true },
+          y: { beginAtZero: true },
         },
       },
     };
 
-    new Chart(this.chartCanvas.nativeElement, config);
+    this.chart = new Chart(this.chartCanvas.nativeElement, config);
+  }
+
+  updateChart() {
+    if (this.chart) {
+      this.chart.data = this.data; 
+      this.chart.update();
+    }
   }
 }
+
+
+// import { Component, Input, OnInit, ViewChild, ElementRef } from '@angular/core';
+// import { Chart, ChartConfiguration, registerables } from 'chart.js';
+
+// @Component({
+//   selector: 'app-bar-chart',
+//   standalone: true,
+//   template: `<canvas #chartCanvas></canvas>`,
+//   styles: [`
+//     canvas {
+//       max-width: 100%;
+//       height: 350px;
+//       margin: auto;
+//     }
+//   `]
+// })
+// export class BarChartComponent implements OnInit {
+//   @Input() data: { labels: string[]; datasets: { label: string; data: number[]; backgroundColor: string }[] } = {
+//     labels: [],
+//     datasets: [],
+//   };
+//   @ViewChild('chartCanvas', { static: true }) chartCanvas!: ElementRef<HTMLCanvasElement>;
+
+//   constructor() {
+//     Chart.register(...registerables);
+//   }
+
+//   ngOnInit() {
+//     this.renderChart();
+//   }
+
+//   renderChart() {
+//     const config: ChartConfiguration = {
+//       type: 'bar',
+//       data: this.data,
+//       options: {
+//         responsive: true,
+//         maintainAspectRatio: false,
+//         plugins: {
+//           legend: {
+//             position: 'top',
+//           },
+//         },
+//         scales: {
+//           x: {
+//             beginAtZero: true,
+//           },
+//           y: {
+//             beginAtZero: true,
+//           },
+//         },
+//       },
+//     };
+
+//     new Chart(this.chartCanvas.nativeElement, config);
+//   }
+// }
