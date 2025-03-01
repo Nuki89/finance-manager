82b4ba4f03660c62ae773563c8b16297e4c40bcf
rud0lph89 <z.spiler46@gmail.com>
2025-03-01 17:30:08 +0100

    update: refactoring (hopefully all works..)

--- frontend/src/app/routes/home/components/expense-summary/expense-summary.component.ts
+++ frontend/src/app/routes/home/components/expense-summary/expense-summary.component.ts
@@ -9,53 +9,52 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './expense-summary.component.html',
-  styleUrl: './expense-summary.component.css'
+  styleUrls: ['./expense-summary.component.css']
 })
 export class ExpenseSummaryComponent {
-  totalExpense: number = 0;
-  selectedView: string = 'month';
-  viewSubscription: any;
+  public totalExpense: number = 0;
+  public selectedView: string = 'month';
 
-  loading: boolean = true;
+  private viewSubscription: any;
+  private loading: boolean = true;
 
   constructor(
-      private expenseService: ExpenseService,
-      private toggleViewService: ToggleViewService
-    ) {}
-  
-    ngOnInit() {
-      this.viewSubscription = this.toggleViewService.viewMode$.subscribe(view => {
-        this.selectedView = view;
-        this.fetchExpenseData();
-      });
-  
-    }
-  
-    fetchExpenseData(): void {
-      this.loading = true; 
-  
-      const totalExpenseFetcher =
-        this.selectedView === 'Yearly'
-          ? this.expenseService.getLastYearExpense()
-          : this.expenseService.getLastMonthExpense();
-  
-      totalExpenseFetcher.subscribe(
-        (data: any) => {
-          this.totalExpense = data.reduce((sum: any, item: { total_amount: any }) => sum + item.total_amount, 0);
-          this.checkLoadingComplete();
-        },
-        (error) => {
-          console.error(`Error fetching ${this.selectedView} total income:`, error);
-          this.totalExpense = 0;
-          this.checkLoadingComplete();
-        }
-      );
-    }
-  
-    checkLoadingComplete(): void {
-      if (this.totalExpense !== 0) {
-        this.loading = false;
+    private expenseService: ExpenseService,
+    private toggleViewService: ToggleViewService
+  ) {}
+
+  ngOnInit() {
+    this.viewSubscription = this.toggleViewService.viewMode$.subscribe(view => {
+      this.selectedView = view;
+      this.fetchExpenseData();
+    });
+  }
+
+  public fetchExpenseData(): void {
+    this.loading = true; 
+
+    const totalExpenseFetcher =
+      this.selectedView === 'Yearly'
+        ? this.expenseService.getLastYearExpense()
+        : this.expenseService.getLastMonthExpense();
+
+    totalExpenseFetcher.subscribe(
+      (data: any) => {
+        this.totalExpense = data.reduce((sum: any, item: { total_amount: any }) => sum + item.total_amount, 0);
+        this.checkLoadingComplete();
+      },
+      (error) => {
+        console.error(`Error fetching ${this.selectedView} total income:`, error);
+        this.totalExpense = 0;
+        this.checkLoadingComplete();
       }
+    );
+  }
+
+  private checkLoadingComplete(): void {
+    if (this.totalExpense !== 0) {
+      this.loading = false;
     }
+  }
 
 }
