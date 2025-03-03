12d80023a87df8e790ccb4e8d556497e755c2103
rud0lph89 <z.spiler46@gmail.com>
2025-03-03 22:46:12 +0100

    feat: added export income/expense pdf to report component/changed look

--- frontend/src/app/routes/transactions/reports/reports.component.ts
+++ frontend/src/app/routes/transactions/reports/reports.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ReportService } from '../../../shared/services/api/report.service';
 import { CommonModule } from '@angular/common';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { blob } from 'stream/consumers';
 
 @Component({
   selector: 'app-reports',
@@ -18,14 +19,34 @@ export class ReportsComponent {
     private sanitizer: DomSanitizer
   ) { }
 
-  loadPdf() {
-    this.reportService.exportPdf().subscribe(blob => {
+  loadVSsummaryPdf() {
+    this.reportService.exportVsPdf().subscribe(blob => {
       const pdfBlob = new Blob([blob], { type: 'application/pdf' });
       const pdfObjectUrl = URL.createObjectURL(pdfBlob);
       window.open(pdfObjectUrl, '_blank');
 
       this.pdfUrl = this.sanitizer.bypassSecurityTrustResourceUrl(pdfObjectUrl);
 
+    });
+  }
+
+  loadIncomeSummaryPdf() {
+    this.reportService.exportIncomePdf().subscribe(blob => {
+      const pdfBlob = new Blob([blob], { type: 'application/pdf'});
+      const pdfObjectUrl = URL.createObjectURL(pdfBlob);
+      window.open(pdfObjectUrl, '_blank');
+
+      this.pdfUrl = this.sanitizer.bypassSecurityTrustResourceUrl(pdfObjectUrl);
+    });
+  }
+
+  loadExpenseSummaryPdf() {
+    this.reportService.exportExpensePdf().subscribe(blob => {
+      const pdfBlob = new Blob([blob], { type: 'application/pdf'});
+      const pdfObjectUrl = URL.createObjectURL(pdfBlob);
+      window.open(pdfObjectUrl, '_blank');
+
+      this.pdfUrl = this.sanitizer.bypassSecurityTrustResourceUrl(pdfObjectUrl);
     });
   }
 
