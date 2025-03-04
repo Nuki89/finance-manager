a9782e90500dccdb8484b4fd6e49754da8a3f411
rud0lph89 <z.spiler46@gmail.com>
2025-03-04 22:47:28 +0100

    feat: implemented first getSaving subs

--- frontend/src/app/routes/transactions/savings/savings.component.ts
+++ frontend/src/app/routes/transactions/savings/savings.component.ts
@@ -1,12 +1,52 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+import { Saving } from '../../../shared/models/saving.model';
+import { Subject, takeUntil } from 'rxjs';
+import { SavingService } from '../../../shared/services/api/saving.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-savings',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './savings.component.html',
-  styleUrl: './savings.component.css'
+  styleUrls: ['./savings.component.css']
 })
 export class SavingsComponent {
+  public savings: Saving[] = [];
+
+  private destroy$ = new Subject<void>();
+
+  constructor(
+    private savingService: SavingService,
+    private dialog: MatDialog,
+    private toastr: ToastrService,
+  ) {}
+
+  ngOnInit(): void {
+    this.fetchSavings();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  private fetchSavings(): void {
+    this.savingService.getSaving()
+    .pipe(takeUntil(this.destroy$))
+    .subscribe({
+      next: (data) => {
+        this.savings = data;
+        console.log('Savings:', this.savings);
+      },
+      error: (error) => {
+        this.toastr.error('Failed to load savings. Please try again.');
+        console.error('Error fetching savings:', error);
+      }
+    })
+  }
 
 }
