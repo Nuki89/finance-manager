b39a6d8f01da2b85057f29029713b0101ceb1b19
rud0lph89 <z.spiler46@gmail.com>
2025-03-11 20:48:07 +0100

    feat: updated register/added two fields/implemented connection /w services

--- frontend/src/app/routes/auth/register/register.component.ts
+++ frontend/src/app/routes/auth/register/register.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { cryptoBtc } from '@ng-icons/cryptocurrency-icons';
+import { bootstrapEye, bootstrapEyeSlash, bootstrapExclamationDiamond } from '@ng-icons/bootstrap-icons';
+import { AuthService } from '../../../shared/services/auth/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -11,8 +13,38 @@ import { cryptoBtc } from '@ng-icons/cryptocurrency-icons';
   imports: [CommonModule, FormsModule, RouterModule, NgIcon],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
-  viewProviders : [provideIcons({ cryptoBtc })]
+  viewProviders : [provideIcons({ cryptoBtc, bootstrapEye, bootstrapEyeSlash, bootstrapExclamationDiamond })]
 })
 export class RegisterComponent {
-  title = "Coiny"
+  public showPassword: boolean = false;
+  public isInvalid: boolean = false;
+  public username: string = '';
+  public password: string = '';
+  public password2: string = '';
+  public email: string = '';
+  public title = "Coiny"
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  public onSubmit() {
+    console.log(this.email, this.username, this.password, this.password2)
+    this.authService.register(this.email, this.username, this.password, this.password2).subscribe(
+      () => {
+        this.isInvalid = false;
+        this.router.navigate(['/login']);
+      },
+      error => {
+        this.isInvalid = true;
+        console.error('Register failed:', error);
+      }
+    );
+  }
+
+  public togglePassword(): void {
+    this.showPassword = !this.showPassword;
+  }
+
 }
