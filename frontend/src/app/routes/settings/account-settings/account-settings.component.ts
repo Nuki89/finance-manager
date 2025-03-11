a056faf7d824b62e5255a80b4d6e057b2203d008
rud0lph89 <z.spiler46@gmail.com>
2025-03-11 21:26:29 +0100

    update: added implementation inside settings/acc

--- frontend/src/app/routes/settings/account-settings/account-settings.component.ts
+++ frontend/src/app/routes/settings/account-settings/account-settings.component.ts
@@ -1,12 +1,56 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AuthService } from '../../../shared/services/auth/auth.service';
+import { Subject, takeUntil } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { FormsModule } from '@angular/forms';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { bootstrapCamera } from '@ng-icons/bootstrap-icons';
+
 
 @Component({
   selector: 'app-account-settings',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, NgIcon],
   templateUrl: './account-settings.component.html',
-  styleUrl: './account-settings.component.css'
+  styleUrls: ['./account-settings.component.css'],
+  viewProviders : [provideIcons({ bootstrapCamera })]
 })
 export class AccountSettingsComponent {
+  public name: string = '';
+  public surname: string = '';
+  public username: string = '';
+  public email: string = '';
+
+  public profile: any = [];
+  private destroy$ = new Subject<void>();
+
+  constructor(
+    private authService: AuthService,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnInit(): void {
+    this.getFetchProfie();
+  }
+
+  private getFetchProfie(): void {
+      this.authService.getProfile()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (data) => {
+          this.profile = data;
+          console.log('Profile:', this.profile);
+          this.name = this.profile.name;
+          this.surname = this.profile.surname;
+          this.email = this.profile.email;
+          this.username = this.profile.username;
+        },
+        error: (error) => {
+          this.toastr.error('Failed to load savings. Please try again.');
+          console.error('Error fetching savings:', error);
+        }
+      })
+    }
 
 }
