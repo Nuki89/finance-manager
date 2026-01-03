aca3edc1b5cc8007164fcd42cf8e4047997399ac
rud0lph89 <z.spiler46@gmail.com>
2026-01-03 17:12:22 +0100

    chore: start-scripts

--- backend/backend.sh
+++ backend/backend.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# Navigate to the app directory
 cd /app
 
-# Apply database migrations
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 
-# Collect static files (optional for dev)
 python manage.py collectstatic --noinput
 
-# Use runserver for dev, gunicorn for prod
 if [ "$MODE" = "dev" ]; then
     echo "Running in DEVELOPMENT mode"
     exec python manage.py runserver 0.0.0.0:8000
--- frontend/frontend.sh
+++ frontend/frontend.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
 export PATH="/root/.npm-global/bin:$PATH"
-
-# Navigate to the app directory
 cd /app
-
-# Install dependencies
 npm install
 
-# Build Angular app if in production mode
 if [ "$NODE_ENV" = "production" ]; then
-    echo "Running in PRODUCTION mode"
-    
-    # Start NGINX
+    echo "Running in PRODUCTION mode"  
     exec nginx -g "daemon off;"
 else
     echo "Running in DEVELOPMENT mode"
-    
-    # Start Angular dev server
     exec ng serve --host 0.0.0.0 --port 4200
 fi
